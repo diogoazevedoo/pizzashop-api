378bfd709aba1c62badddcfa2493a2b18f5430b9
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-02 17:32:14 +0000

    Route: created authenticate from link route with jwt token generation and redirection

---
+++ src/http/auth.ts
@@ -0,0 +1,16 @@
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import { Elysia, t } from 'elysia'
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
