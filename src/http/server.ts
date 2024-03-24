c3d0864a1104e27af77d83966f914463af872d8b
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-24 19:00:38 +0000

    route: add list restaurant orders based on page index

--- src/http/server.ts
+++ src/http/server.ts
@@ -10,6 +10,7 @@ import { approveOrder } from './routes/approve-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { deliverOrder } from './routes/deliver-order'
 import { cancelOrder } from './routes/cancel-order'
+import { getOrders } from './routes/get-orders'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -23,6 +24,7 @@ const app = new Elysia()
   .use(dispatchOrder)
   .use(deliverOrder)
   .use(cancelOrder)
+  .use(getOrders)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
